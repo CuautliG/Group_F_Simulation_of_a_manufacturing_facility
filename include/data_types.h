8f82b6ef5c836e914141fc9ea9f10cfe0c75fcdc
CuautliG <58396559+CuautliG@users.noreply.github.com>
2020-05-29 09:59:56 -0400

    data_types header modified

--- include/data_types.h
+++ include/data_types.h
@@ -1,12 +1,40 @@
 #ifndef DATA_TYPES_H_INCLUDED
 #define DATA_TYPES_H_INCLUDED
 
-struct CONFIG{
-        int option1;        //Initialize model choice
-        int option2;        //Initialize the input method
+MAX_SIMULATION_LENGTH=500;
+
+struct CONFIG{              //Temporal function to provide the initial values
+        int both_models;    //If the simulator will run once or twice
+        int model_type;     //Model type of the simulator
+        char read_generate; //Read data or generate data
         int simlength;      //Simulation length
         float lolimit;      //Lower limit
         float uplimit;      //Upper limit
+};
+
+struct INSPECTOR{           //Inspector instructor
+    char *id;               //Id of inspector
+    char *name;             //Name of the inspector
+    int type;               //Type of component he inspects
+    float pro_time;         //Processing time of the inspector
+    float idle_time;        //Idle time of the inspector
+};
+
+struct STATE{               //
+    int model;
+    int sim_length;
+    int iteration;
+    int produced_prod;
+    struct INSPECTOR inspectors;
+};
+
+struct PTIME{               //Processing time function
+    float *comp1;           //Processing time of the component 1
+    float *comp2;           //Processing time of the component 2
+    float *comp3;           //Processing time of the component 3
+    float *ws1;             //Processing time of the working station 1
+    float *ws2;             //Processing time of the working station 2
+    float *ws3;             //Processing time of the working station 3
 };
 
 #endif // DATA_TYPES_H_INCLUDED
