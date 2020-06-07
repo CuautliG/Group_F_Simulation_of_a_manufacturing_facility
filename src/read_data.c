7ec516ed49c424b644e6e4fcf6bc78d3f936837d
panakou <65472097+panakou@users.noreply.github.com>
2020-06-07 10:14:10 -0400

    New version of the read_data function
    I updated the read data function.

--- src/read_data.c
+++ src/read_data.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include "../include/read_data.h"
 
-//#include "../include/read_data.h"
-
-//int read_data(*char input_directory, char *config_file, struct **my_ptime)
+int read_data(char *input_directory, struct CONFIG my_config, struct PTIME *my_ptime){
+    printf("Input directory: %s\n",input_directory);
+    printf("Model type: %d\n",my_config.model_type);
 
 int lengthc1; // number of component C1 supplied for simulation
 int lengthc2; // number of component C2 supplied for simulation
@@ -13,31 +15,9 @@ int lengthw1; // number of time intervals for assembling P1 for simulation
 int lengthw2; // number of time intervals for assembling P2 for simulation
 int lengthw3; // number of time intervals for assembling P3 for simulation
 
-int read_client_files()
+int simlength = my_config.sim_length;
+if (my_config.read_generate == 'r')
 {
-    float *data(float *c, char STR[20], int *length){
-        FILE *ins= fopen(STR,"r");
-        int i =0;
-        char oneline[8];
-
-        while (!feof(ins)) {
-            fscanf(ins,"%8s",oneline);
-            c[i]=strtof(oneline,NULL);
-            i++;
-        }
-
-        *length=i-1;
-        fclose(ins);
-
-        return c;
-    }
-
-    float *c1; // address of processing time C1
-    float *c2; // address of processing time C2
-    float *c3; // address of processing time C3
-    float *w1; // address of processing time P1
-    float *w2; // address of processing time P2
-    float *w3; // address of processing time P3
 
     float array1[5000]; // process time of inspector 1
     float array2[5000]; // process time of inspector 2 for C2
@@ -46,16 +26,60 @@ int read_client_files()
     float array5[5000]; // process time of workstation 2
     float array6[5000]; // process time of workstation 3
 
+    char long_str[6][23];
+    char* input_directory=  "./data/input/";
+    char* components[] = {"ser1.dat", "ser2.dat", "ser3.dat", "ws1.dat", "ws2.dat", "ws3.dat"};
+
+    for (int i=0; i<6; i++)
+    {
+    strncat(long_str[i], input_directory, 13);
+    strncat(long_str[i], components[i], 20);
+    }
     // retrieve data from files
-    c1=data(array1,"servinsp1.dat", &lengthc1);
-    c2=data(array2,"servinsp22.dat", &lengthc2);
-    c3=data(array3,"servinsp23.dat", &lengthc3);
-    w1=data(array4,"ws1.dat", &lengthw1);
-    w2=data(array5,"ws2.dat", &lengthw2);
-    w3=data(array6,"ws3.dat", &lengthw3);
+    my_ptime.comp1=data(array1,long_str[0], &lengthc1);
+    my_ptime.comp2=data(array2,long_str[1], &lengthc2);
+    my_ptime.comp3=data(array3,long_str[2], &lengthc3);
+    my_ptime.ws1=data(array4,long_str[3], &lengthw1);
+    my_ptime.ws2=data(array5,long_str[4], &lengthw2);
+    my_ptime.ws3=data(array6,long_str[5], &lengthw3);
 }
 
-int main()
+else if (my_config.read_generate == 'g')
+        {
+        my_ptime.comp1 = generate_random;
+        my_ptime.comp2 = generate_random;
+        my_ptime.comp3 = generate_random;
+        my_ptime.ws1 = generate_random;
+        my_ptime.ws2 = generate_random;
+        my_ptime.ws3 = generate_random;
+    }
+}
+
+float *data(float *c, char STR[20], int *length){
+            FILE *ins= fopen(STR,"r");
+            int i =0;
+            char oneline[8];
+
+            while (!feof(ins)) {
+                fscanf(ins,"%8s",oneline);
+                c[i]=strtof(oneline,NULL);
+                i++;
+            }
+
+            *length=i-1;
+            fclose(ins);
+            return c;
+        }
+
+float generate_random()
 {
-    read_client_files();
+    float upper = my_config.uplimit;
+    float lower = my_config.lolimit;
+    float ran_num_generator;
+    float rand_numb [simlength];
+    for(int i = 0; i<simlength; i++)
+    {
+    ran_num_generator = lower + (float)rand() / (RAND_MAX / (upper - lower + 1) + 1);
+    rand_numb[i] = ran_num_generator;
+    }
 }
