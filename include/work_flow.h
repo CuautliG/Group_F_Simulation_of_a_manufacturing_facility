2fad1bb82217048d4773b09857ae92f6e0070eed
ngochai1604 <56520980+ngochai1604@users.noreply.github.com>
2020-06-16 16:57:56 -0400

    Update work_flow.h

--- include/work_flow.h
+++ include/work_flow.h
@@ -1,40 +1,9 @@
-#ifndef DATA_TYPES_H_INCLUDED
-#define DATA_TYPES_H_INCLUDED
+#ifndef WORK_FLOW_H_INCLUDED
+#define WORK_FLOW_H_INCLUDED
 
-#define MAX_SIMULATION_LENGTH 500
+int work_flow(struct CONFIG *my_config,struct PTIME *my_ptime,struct STATE *my_state);
+float clockfcn(float i1ta, float i2ta, float w1ta, float w2ta, float w3ta);
+int *counters(float i1ta,float i2ta,float w1ta,float w2ta,float w3ta,int b11, int b21, int b22, int b31,int b33,int i2type);
+int *buffer1(float b11,int b21,int b31,int b22, int b33, int opt);
 
-struct CONFIG{              //Temporal function to provide the initial values
-    int both_models;        //If the simulator will run once or twice - 1:once / 2:twice
-    int model_type;         //Model type of the simulator - 1:old /2:new
-    char read_generate;     //Read data or generate data
-    int sim_length;          //Simulation length
-    float lolimit;          //Lower limit
-    float uplimit;          //Upper limit
-};
-typedef struct CONFIG Config;
-struct INSPECTOR{           //Inspector instructor
-    char *id;               //Id of inspector
-    char *name;             //Name of the inspector
-    float *idle_time;        //Idle time of the inspector
-};
-typedef struct INSPECTOR Inspector;
-struct STATE{                   //State of simulator
-    int model;                  //Simulation model ** not necessary ** since *state= old model , *(state+1)=new model //fixed
-    float *simulation_time;             //Simulation length *simulation_time
-    int *produced_prod;       //Produced products
-    int *used_component;      //Used components - *not necessary* *suggest removal*
-    int *unused_component;    //Unused components
-    struct INSPECTOR *inspector1;//Inspector one
-    struct INSPECTOR *inspector2;//Inspector two
-};
-typedef struct STATE State;
-struct PTIME{               //Processing time function
-    float *comp1;           //Processing time of the component 1
-    float *comp2;           //Processing time of the component 2
-    float *comp3;           //Processing time of the component 3
-    float *ws1;             //Processing time of the working station 1
-    float *ws2;             //Processing time of the working station 2
-    float *ws3;             //Processing time of the working station 3
-};
-typedef struct PTIME Ptime;
 #endif // WORK_FLOW_H_INCLUDED
