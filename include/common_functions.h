aa6dc815fee9311a22a085e567b856c531589573
CuautliG <58396559+CuautliG@users.noreply.github.com>
2020-06-18 13:28:01 -0400

    common functions header

---
+++ include/common_functions.h
@@ -0,0 +1,7 @@
+#ifndef COMMON_FUNCTIONS_H_INCLUDED
+#define COMMON_FUNCTIONS_H_INCLUDED
+
+void print_state(struct STATE *my_state);
+void print_ptime(struct CONFIG *my_config, struct PTIME *my_ptime);
+
+#endif // COMMON_FUNCTIONS_H_INCLUDED
