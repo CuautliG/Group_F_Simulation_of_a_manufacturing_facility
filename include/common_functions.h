6ab0b88b8a5a6dbee13a6bbf747d0b5184eec325
CuautliG <58396559+CuautliG@users.noreply.github.com>
2020-06-22 17:07:06 -0400

    Update common_functions.h

--- include/common_functions.h
+++ include/common_functions.h
@@ -1,5 +1,6 @@
 #ifndef COMMON_FUNCTIONS_H_INCLUDED
 #define COMMON_FUNCTIONS_H_INCLUDED
+#include "data_types.h"
 
 void print_state(struct STATE *my_state);
 void print_ptime(struct CONFIG *my_config, struct PTIME *my_ptime);
