fce1066bd37dc8effe9a61f19aa35f472d2f442f
CuautliG <58396559+CuautliG@users.noreply.github.com>
2020-06-22 18:33:42 -0400

    Update export_files.h

--- include/export_files.h
+++ include/export_files.h
@@ -1,8 +1,9 @@
 #ifndef EXPORT_FILES_H_INCLUDED
 #define EXPORT_FILES_H_INCLUDED
 
+int export_analysis(struct STATE *my_state , char *file_name);
 int export_files(struct CONFIG *my_config, struct STATE *my_state, char *output_directory);
-int export_csv(struct STATE *my_state , char *file_name);
+int export_csv(struct STATE *my_state , char *file_name, int both_models);
 int write_file(struct STATE *my_state, FILE *file);
 
 #endif // EXPORT_FILES_H_INCLUDED
