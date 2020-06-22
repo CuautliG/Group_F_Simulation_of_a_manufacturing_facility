16a8a9f39d594af75ee4c79e2de5774bd005644f
CuautliG <58396559+CuautliG@users.noreply.github.com>
2020-06-22 16:43:51 -0400

    Delete common_functions_test.h

--- test/include/common_functions_test.h
+++
@@ -1,6 +0,0 @@
-#ifndef COMMON_FUNCTIONS_TEST_H_INCLUDED
-#define COMMON_FUNCTIONS_TEST_H_INCLUDED
-
-void set_state_test(struct CONFIG *my_config,struct STATE *my_state);
-
-#endif // COMMON_FUNCTIONS_TEST_H_INCLUDED
