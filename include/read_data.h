95bf92cf5d6b728012b31c865f9d54bb6c12a06f
panakou <65472097+panakou@users.noreply.github.com>
2020-06-22 16:59:00 -0400

    Update read_data.h

--- include/read_data.h
+++ include/read_data.h
@@ -1,4 +1,10 @@
 #ifndef READ_DATA_H_INCLUDED
 #define READ_DATA_H_INCLUDED
 
+int read_data(char *input_directory, struct CONFIG *my_config, struct PTIME *my_ptime);
+float *data(float *times, char STR[20], int *length);
+float *generate_random(struct CONFIG *my_config,float *rand_numb);
+int read_client_files(char *input_directory, struct PTIME* my_ptime,int length_files);
+int generate_random_time (char *input_directory, struct CONFIG *my_config, struct PTIME* my_ptime);
+
 #endif // READ_DATA_H_INCLUDED
