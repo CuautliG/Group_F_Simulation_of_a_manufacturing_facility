dbf40a7164411690cc79f07cf1b32f4cb00a7619
Charlesemesobum <58150287+Charlesemesobum@users.noreply.github.com>
2020-06-23 13:00:41 -0400

    Update read_config.h

--- include/read_config.h
+++ include/read_config.h
@@ -1,10 +1,8 @@
 #ifndef READ_CONFIG_H_INCLUDED
 #define READ_CONFIG_H_INCLUDED
 
-
+int read_config(char *config_file, struct CONFIG *my_config);
 int split_key_value(char *input_line, char *key_value[]);
 int parse_config(char *config_file, struct CONFIG *my_config);
-int read_config(char *config_file, struct CONFIG *my_config);
-
 
 #endif // READ_CONFIG_H_INCLUDED
