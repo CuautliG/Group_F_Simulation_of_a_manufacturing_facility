9a698daa80f87e1bdbdfe3e42bc742dacab9984d
CuautliG <58396559+CuautliG@users.noreply.github.com>
2020-06-23 12:51:55 -0400

    Second Release

--- test/src/main_test.c
+++ test/src/main_test.c
@@ -2,15 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "../../include/data_types.h"
 #include "../include/main_test.h"
-#include "../include/data_types_test.h"
+#include "../include/read_config_test.h"
 #include "../include/read_data_test.h"
 #include "../include/work_flow_test.h"
 #include "../include/export_files_test.h"
-#include "../include/common_functions_test.h"
 
-//Run Simulator
-int main_test() {
+int main() {
+
+    printf("------------- Testing read_config -------------\n");
+    read_config_test();
+
+    printf("------------- Testing read_data -------------\n");
+    read_data_test();
 
     printf("------------- Testing work_flow -------------\n");
     work_flow_test();
