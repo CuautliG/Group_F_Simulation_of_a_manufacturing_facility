af2f6b314a20fdd49476229bb7617f7dc8fa6dba
panakou <65472097+panakou@users.noreply.github.com>
2020-06-23 12:48:03 -0400

    Update read_data.h
    This is my last version of read_data.h

--- test/include/read_data_test.h
+++ test/include/read_data_test.h
@@ -1,6 +1,11 @@
 #ifndef READ_DATA_TEST_H_INCLUDED
 #define READ_DATA_TEST_H_INCLUDED
 
-
+int read_data_test(void);
+int test_error_1(void);
+int test_error_2(void);
+int test_error_3(void);
+int test_error_4(void);
+int test_no_error(void);
 
 #endif // READ_DATA_TEST_H_INCLUDED
