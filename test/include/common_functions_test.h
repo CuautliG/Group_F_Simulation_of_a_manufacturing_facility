77aba79dad9a356c76bfbbc413f3fe1bcc66faf8
CuautliG <58396559+CuautliG@users.noreply.github.com>
2020-06-23 12:54:10 -0400

    Second Release

--- test/include/common_functions_test.h
+++ test/include/common_functions_test.h
@@ -3,11 +3,11 @@
 
 void set_state_test1(struct CONFIG *my_config,struct STATE *my_state);
 void set_state_test2(struct CONFIG *my_config,struct STATE *my_state);
-struct WORK_FLOW_TEST work_flow_error0();
-struct WORK_FLOW_TEST work_flow_error1();
-struct WORK_FLOW_TEST work_flow_error2();
-struct CLOCKFCN_TEST work_flow_error3();
-struct COUNTERS_TEST work_flow_error4();
-struct BUFFER1_TEST work_flow_error5();
+struct WORK_FLOW_TEST work_flow_error0(void);
+struct WORK_FLOW_TEST work_flow_error1(void);
+struct WORK_FLOW_TEST work_flow_error2(void);
+struct CLOCKFCN_TEST work_flow_error3(void);
+struct COUNTERS_TEST work_flow_error4(void);
+struct BUFFER1_TEST work_flow_error5(void);
 
 #endif // COMMON_FUNCTIONS_TEST_H_INCLUDED
